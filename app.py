0eac8c6584548e0ea0fcb85cdc9507470b2df931
Santos Enoque <37243334+Santos-Enoque@users.noreply.github.com>
2020-04-01 20:22:25 +0300

    add code for whatsapp bot

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
-import os
-from flask import Flask
-from flask_restful import Api
-from flask_jwt import JWT
-from resources.user import UserRegister
-from security import authenticate, identity
-from resources.items import Item, ItemList
-from resources.store import Store, StoreList
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL, sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# To allow flask propagating exception even if debug is set to false on app
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
-api = Api(app)
 
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
 
-# app.config['JWT_AUTH_URL_RULE'] = '/login'
-# config JWT auth key name to be 'email' instead of default 'username' -
-# WILL USE phoneNumber in our case
-# app.config['JWT_AUTH_USERNAME_KEY'] = 'email' # change from email
-#  to phoneNumber
-jwt = JWT(app, authenticate, identity)
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("Disse: {} (em breve estaremos dando atualizações sobre corona em Mozambique!! Ability Team)".format(msg))
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
+    return str(resp)
 
-if __name__ == '__main__':
-    # db.init_app(app)
-    app.run(debug=True)  # important to mention debug=True
+if __name__ == "__main__":
+    app.run(debug=True)
