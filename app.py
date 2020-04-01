667785beb841d3e0c639fab7dd053e0a09c21410
Santos Enoque <37243334+Santos-Enoque@users.noreply.github.com>
2020-04-01 23:17:54 +0300

    another update

--- app.py
+++ app.py
@@ -15,14 +15,12 @@ def sms_reply():
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("Disse: {} (digite 'A' para ter atualizações sobre o covid19 em moçambique \n digite 'B' para saber sobre os métodos de prevenção)".format(msg))
+    resp.message("*Bem vindo ao chatbot demo moçambicano sobre COVID 19* \n\n Obtenha informações e orientações da XXXX sobre o atual surto de doença por coronavírus (COVID-19). \n\n *O que você gostaria de saber sobre coronavírus?* \n\n Responda com um número a qualquer momento para obter as informações mais recentes sobre o tópico:\n 1. Latest numbers \n 2. Protect yourself \n 3. Your questions answered \n 4. Mythbusters \n 5. Travel advice \n 6. News & Press \n 7. Share \n 8. Donate now \n 9. Change language")
 
-    if msg =='A' or  msg =='a':
-         resp.message("existem x casos de covid19 em moçambique")
+    if msg =='1' or  msg ==1:
+         resp.message("Funcionalidade em desenvolvimento")
          return str(resp)
-    elif msg =='B' or  msg =='b':
-         resp.message("Fique em casa!!!")
-         return str(resp)
+   
 
     return str(resp)
 
